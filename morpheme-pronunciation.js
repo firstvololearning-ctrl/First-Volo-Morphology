7f1442cff919799027ea6cbc8c60939d8ee57396
Alexis Sacco <firstvololearning@gmail.com>
2026-08-30 21:13:24 -0400

    Add approved controlled suffix audio batch 2

--- morpheme-pronunciation.js
+++ morpheme-pronunciation.js
@@ -138,9 +138,10 @@
   const ROOTS_BATCH1_AUDIO_KEYS = Object.freeze(["ject", "pos", "put", "rupt", "scrib", "script", "spect", "struct", "tract"]);
   const ROOTS_BATCH2_AUDIO_KEYS = Object.freeze(["ven", "vent", "voc", "act", "aud", "dict", "derm", "terr"]);
   const SUFFIXES_BATCH1_AUDIO_KEYS = Object.freeze(["al", "ance", "ence", "ic", "ist", "ize", "ify"]);
-  const CONTROLLED_CLIP_AUDIO_KEYS = Object.freeze([...PILOT_AUDIO_KEYS, ...PREFIX_AUDIO_KEYS, ...ROOTS_BATCH1_AUDIO_KEYS, ...ROOTS_BATCH2_AUDIO_KEYS, ...SUFFIXES_BATCH1_AUDIO_KEYS]);
-  const CONTROLLED_REVIEW_AUDIO_KEYS = Object.freeze([...CONTROLLED_CLIP_AUDIO_KEYS, ...ROOTS_BATCH2_AUDIO_KEYS, ...SUFFIXES_BATCH1_AUDIO_KEYS]);
-  const CONTROLLED_AUDIO_REVIEW_BATCHES = Object.freeze({ pilot: PILOT_AUDIO_KEYS, prefixes: PREFIX_AUDIO_KEYS, "roots-1": ROOTS_BATCH1_AUDIO_KEYS, "roots-2": ROOTS_BATCH2_AUDIO_KEYS, "suffixes-1": SUFFIXES_BATCH1_AUDIO_KEYS });
+  const SUFFIXES_BATCH2_AUDIO_KEYS = Object.freeze(["ion", "tion", "ment", "ous", "ant", "ent"]);
+  const CONTROLLED_CLIP_AUDIO_KEYS = Object.freeze([...PILOT_AUDIO_KEYS, ...PREFIX_AUDIO_KEYS, ...ROOTS_BATCH1_AUDIO_KEYS, ...ROOTS_BATCH2_AUDIO_KEYS, ...SUFFIXES_BATCH1_AUDIO_KEYS, ...SUFFIXES_BATCH2_AUDIO_KEYS]);
+  const CONTROLLED_REVIEW_AUDIO_KEYS = Object.freeze([...CONTROLLED_CLIP_AUDIO_KEYS, ...ROOTS_BATCH2_AUDIO_KEYS, ...SUFFIXES_BATCH1_AUDIO_KEYS, ...SUFFIXES_BATCH2_AUDIO_KEYS]);
+  const CONTROLLED_AUDIO_REVIEW_BATCHES = Object.freeze({ pilot: PILOT_AUDIO_KEYS, prefixes: PREFIX_AUDIO_KEYS, "roots-1": ROOTS_BATCH1_AUDIO_KEYS, "roots-2": ROOTS_BATCH2_AUDIO_KEYS, "suffixes-1": SUFFIXES_BATCH1_AUDIO_KEYS, "suffixes-2": SUFFIXES_BATCH2_AUDIO_KEYS });
   const PILOT_AUDIO_EXTENSION = "m4a";
   Object.entries(controlledById).forEach(([id, keys]) => keys.forEach((key) => addManifestForm(key, id)));
   Object.entries(controlledByVariant).forEach(([label, key]) => addManifestForm(key, null, `variant:${label}`));
